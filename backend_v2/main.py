8c2d439e582b427b54ce9b14a447493174d1be5d
rasulabduvaitov <rasulabduvaitov@pop-os.localdomain>
2023-04-15 15:57:03 +0500

    render .env

--- backend_v2/main.py
+++ backend_v2/main.py
@@ -6,3 +6,4 @@ if __name__ == '__main__':
     uvicorn.run("code.app.main:app",host='0.0.0.0' ,port=settings.SVC_PORT, reload=True)
 
 
+# export PYTHONPATH=:$/home/rasulabduvaitov/Desktop/uptimoll/backend_v2/code/app/
